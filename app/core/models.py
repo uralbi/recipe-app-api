2785bf4bf98b1dfd52499d1c52dada98edfe51bc
uralbi <ural46@gmail.com>
2023-02-13 19:23:53 -0800

    added recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,5 @@
+
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -35,3 +37,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """ Recipe object. """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    time_minutes = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
