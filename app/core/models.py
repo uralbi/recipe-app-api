d91c7a674f74c3e1d89fe7693436855838fefa03
uralbi <ural46@gmail.com>
2023-02-16 12:13:35 -0800

    added recipe model and test

--- app/core/models.py
+++ app/core/models.py
@@ -47,8 +47,8 @@ class Recipe(models.Model):
     )
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    price = models.IntegerField()
-    time_minutes = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    time_minutes = models.IntegerField()
     link = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
